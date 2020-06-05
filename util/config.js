ed61c66aa4346b77e459f89247595a1ba345ddfe
nightlywinnetou <60691392+nightlywinnetou@users.noreply.github.com>
2020-06-06 00:58:10 +0200

    master

---
+++ util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyDRuibGkbvgWhNMgcCH_TJl47EeiYXv3Ls",
+  authDomain: "erasmus-social.firebaseapp.com",
+  databaseURL: "https://erasmus-social.firebaseio.com",
+  projectId: "erasmus-social",
+  storageBucket: "erasmus-social.appspot.com",
+  messagingSenderId: "904641500506",
+  appId: "1:904641500506:web:228383de0e61c6e3368f63",
+  measurementId: "G-VB9K48MMC5"
+}
